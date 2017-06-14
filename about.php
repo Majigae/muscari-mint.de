986398b0063888babb39982a158cbc37932a1c45
Majigae <helena-ma@outlook.com>
2017-06-14 08:44:25 +0200

    added mobile contact-form

--- about.php
+++ about.php
@@ -8,6 +8,18 @@
 		<h3>About</h3>
 		
 	</div>
+	<div class="middle-content">
+		<div id="mobile-kontakt">
+			<h3>Kontakt</h3>
+			<form method="post" action="kontakt.php">
+				<input type="text" name="name" placeholder="Name"><br>
+				<input type="email" name="email" placeholder="E-Mail"><br>
+				<input type="text" name="betreff" placeholder="Betreff"><br>
+				<textarea name="nachricht" placeholder="Nachricht"></textarea><br>
+				<input type="submit" name="submit" value="Senden">
+			</form>
+		</div>
+	</div>
 </div>
 <div class="right-content">
 	<div class="top-content">
